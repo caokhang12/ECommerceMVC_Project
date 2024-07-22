efebb05da8c5b28dc7c3d62c6ff07b7155795ec5
khang <khangcaopham2003@gmail.com>
2024-07-22 09:40:10 +0700

    thêm side menu sp

---
+++ ECommerceMVC/ViewComponents/SideMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ECommerceMVC.ViewComponents
+{
+    public class SideMenuViewComponent : ViewComponent
+    {
+        private readonly HshopContext _db;
+        public SideMenuViewComponent(HshopContext dbContext) => _db = dbContext;
+
+        public IViewComponentResult Invoke()
+        {
+            var menu = _db.Loais.Select(loai => new LoaiViewModel
+            {
+                MaLoai = loai.MaLoai,
+                TenLoai = loai.TenLoai,
+                SoLuong = loai.HangHoas.Count()
+            });
+            return View(menu);
+        }
+    }
+}
